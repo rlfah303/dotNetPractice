d4c24a53248ae05735da14e0260e33d10b7563ff
rlfah303 <rlarlfah303@gmail.com>
2021-12-07 18:41:02 +0900

    add post model

---
+++ dotNetPractice/dotNetPractice/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace dotNetPractice.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public Post()
+        {
+
+        }
+    }
+}
